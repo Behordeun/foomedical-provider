9cdf3bc27f87f230d9311175066a15e0e115f245
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-01-15 00:14:50 +0100

    Updated credentials

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -7,9 +7,9 @@ export function SignInPage(): JSX.Element {
   const navigate = useNavigate();
   return (
     <SignInForm
-      projectId="daae901c-c8d7-4246-9f24-486147b82224"
-      googleClientId="679052511930-s86ja7j6371gmdsmmgqrdoe1qmbr0aou.apps.googleusercontent.com"
-      onSuccess={() => navigate('/')}
+        projectId="654455ae-918e-4650-9cfd-01ea08ec702b"
+        googleClientId="397236612778-c0b5tnjv98frbo1tfuuha5vkme3cmq4s.apps.googleusercontent.com"
+        onSuccess={() => navigate('/')}
     >
       <Logo size={32} />
       <Text size="lg">Sign in to Foo Provider</Text>
